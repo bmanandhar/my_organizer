a5e45903e6b2c10cc1c0688fb7ee0fb986e4773e
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-01-22 14:30:17 -0800

    getting better

--- public/script/app.js
+++ public/script/app.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     function getQuote(){
     var url= "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
      
-    //API call using Json GET method
+    //API call using Json GET method, instruction given
     $.getJSON(url, function(data){
       
-      console.log("Bijaya's codepen");
+      console.log("Bijaya's Organizer");
       console.log(typeof data);
       console.log("Type of data");
       console.log(data);
@@ -31,13 +31,13 @@ $(document).ready(function(){
       $("#tweet").on("click", function(){
       window.open("https://twitter.com/intent/tweet?text="+quote+"   - "+author);
       });
-    
+      console.log(quote+' - '+author);
       $(".facebook").on("click", function(){
       window.open("https://www.facebook.com/sharer/sharer.php?u="+ link);
     });
-    
-      $("#newQuote").on("click", function(){     
-      getQuote();
+
+    $(".quote").on("click", function(){   
+        getQuote();
     });
     
   });
