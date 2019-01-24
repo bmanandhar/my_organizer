ecae1c271bb45823398e5ebce8c3b485c7666604
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-01-23 16:01:13 -0800

    news room is open now

--- public/script/news.js
+++ public/script/news.js
@@ -1,15 +1,26 @@
+
 $(document).ready(function(){
-
-    var url = 'https://newsapi.org/v2/top-headlines?' +
-    'country=us&' +
-    'apiKey=307652aa8aa44bf0869e6147a272b466';
-    var req = new Request(url);
-    fetch(req)
+    const url = 'https://newsapi.org/v2/top-headlines?'+
+                'country=us&'+
+                'apiKey=307652aa8aa44bf0869e6147a272b466';
+    const req = new Request(url);
+    fetch(url)
     .then(function(response) {
-    console.log(response.json());
-        
+        return response.json()
+    }).then((Jsonresponse)=>{
+        let 
+            all_news = Jsonresponse.articles  
+            length = all_news.length
+        console.log(all_news);
 
-
-
+        for (let i=0; i<length; i++) {
+            $('.news-content').append(`
+                <p>${all_news[i].publishedAt}</p>
+                <p>${all_news[i].source.name}</P>
+                <p>${all_news[i].content}</p>
+                <hr>`
+            )
+        }
     })
+});
     
