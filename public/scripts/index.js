826a7d4f02b633f589271a5ad8aa4750cd8b99a4
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-02-18 13:55:40 -0800

    adds some changes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -77,7 +77,7 @@ $(document).ready(function(){
         });
     //Posting data straight to facebook
     $(".facebook").on("click", function(){
-        window.open(`https://www.facebook.com/sharer/sharer.php?u=${quote} - ${author}`);
+        window.open("https://www.facebook.com/sharer/sharer.php?u=" + quote + author);
     });
       $(".quote").on("click", function(){   
           getQuote();
