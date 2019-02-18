2d4e8d9785ba08028b524782ebb66e8de65ce34c
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-02-18 11:09:27 -0800

    adds some changes

--- public/scripts/ipinfo.js
+++ public/scripts/ipinfo.js
@@ -16,10 +16,16 @@ function getIpInfo(){
       city = response.city;
       region = response.region;
       loc = response.loc;
+      lat_long = loc.split(",");
+      console.log(lat_long);
   
       $(".ip").html(`IP Address: ${ip}`);
       $(".city").html(`City: ${city}`);
       $(".region").html(`Region: ${region}`);
+      $(".lat").html(`Latitude: ${lat_long[0]}`);
+      $(".long").html(`Longitude: ${lat_long[1]}`);
+
+
       $(".loc").html(`Lat / Long: ${loc}`);
     }
   })
